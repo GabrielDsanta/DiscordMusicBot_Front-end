e80070339df5f8654ac457f5654d876689a77f8b
GabrielDSanta <eloviveiros@hotmail.com>
2023-01-02 15:09:57 -0300

    Adicionando botão CreatePlaylist

--- src/components/MyPlaylist/index.tsx
+++ src/components/MyPlaylist/index.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
+import { PlusCircle } from "phosphor-react";
 import { MusicContext } from "../../contexts/MusicContext";
 import { MusicCard } from "../MusicCard";
 import { ContainerPlaylistCard } from "../PlaylistCard/styles";
+import { ContainerContent, CreatePlaylistButton } from "./styles";
 
 
 
@@ -11,20 +13,27 @@ export function MyPlaylist() {
 
 
     return (
-        <ContainerPlaylistCard>
-            <h1>My Playlist</h1>
-            {musicInMyPlaylist.map((item) => {
-                return (
-                    <MusicCard
-                        imageSrc={item.imageSrc}
-                        artist={item.artist}
-                        minutesDurationMusic={item.minutesDurationMusic}
-                        secondsDurationMusic={item.secondsDurationMusic}
-                        musicName={item.musicName}
-                        icon="Delete"
-                    />
-                )
-            })}
-        </ContainerPlaylistCard>
+        <ContainerContent>
+            <CreatePlaylistButton>
+                <PlusCircle  weight="fill" size={28} />
+                Create Playlist
+            </CreatePlaylistButton>
+
+            <ContainerPlaylistCard>
+                <h1>My Playlist</h1>
+                {musicInMyPlaylist.map((item) => {
+                    return (
+                        <MusicCard
+                            imageSrc={item.imageSrc}
+                            artist={item.artist}
+                            minutesDurationMusic={item.minutesDurationMusic}
+                            secondsDurationMusic={item.secondsDurationMusic}
+                            musicName={item.musicName}
+                            icon="Delete"
+                        />
+                    )
+                })}
+            </ContainerPlaylistCard>
+        </ContainerContent>
     )
 }

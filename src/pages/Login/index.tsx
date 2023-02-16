b5cc6f5bd4d9124f379f133ef8c9456fb111fde2
Arthur Rosch <rosch.arthur@gmail.com>
2023-02-15 23:23:01 -0300

    Fazendo login com Spotify

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,20 +6,34 @@ import {
   ContainerLogin,
   FormLogin,
   InputsLogin,
+  Error,
   ButtonLogin,
   ButtonLoginSpotify,
 } from './styles'
 import photo from '../.././assets/image 1.png'
 
 const newLoginFormValidationSchema = zod.object({
-  emailUser: zod.string().email().min(1, 'Email Invalido'),
-  passwordUser: zod.string().min(1, 'Senha Invalida'),
+  emailUser: zod
+    .string()
+    .email('This is not a valid email.')
+    .min(1, { message: 'Password invalid' })
+    .refine((e) => e === 'abcd@fg.com', 'This email is not in our database'),
+
+  passwordUser: zod
+    .string()
+    .min(1, { message: 'Password invalid' })
+    .refine((e) => e === 'abcd@fg.com', 'This password is not in our database'),
 })
 
 type newLoginFormData = zod.infer<typeof newLoginFormValidationSchema>
 
 export function Login() {
-  const { register, handleSubmit, reset } = useForm<newLoginFormData>({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<newLoginFormData>({
     resolver: zodResolver(newLoginFormValidationSchema),
     defaultValues: {
       emailUser: '',
@@ -27,38 +41,46 @@ export function Login() {
     },
   })
 
+  function handleLogin(data: newLoginFormData) {
+    console.log(data)
+    reset()
+  }
+
   return (
     <ContainerLogin>
       <img src={photo} alt="" />
-      <FormLogin>
+      <FormLogin onSubmit={handleSubmit(handleLogin)}>
         <p>Welcome back</p>
         <h1>Login to your account</h1>
         <InputsLogin>
           <label htmlFor="emailUser">Email</label>
           <input
-            required
             type="email"
             id="emailUser"
+            {...register('emailUser')}
             placeholder="John.snow@gmail.com"
           />
+          {errors.emailUser && <Error>{errors.emailUser.message}</Error>}
           <label htmlFor="passwordUser">Password</label>
           <input
-            required
             type="password"
             id="passwordUser"
+            {...register('passwordUser')}
             placeholder="JohnSnowPassword"
           />
-
+          {errors.passwordUser && <Error>{errors.passwordUser.message}</Error>}
           <footer>
             <span>Remember me</span>
             <a href="#">Forgot password?</a>
           </footer>
         </InputsLogin>
-        <ButtonLogin type="submit">Login now</ButtonLogin>
-        <ButtonLoginSpotify type="submit">
-          <SpotifyLogo size={32} color="#00B37E" weight="fill" /> Or sign-in
-          with Spotify
-        </ButtonLoginSpotify>
+        <ButtonLogin>Login now</ButtonLogin>
+        <a href="http://localhost:8080/login">
+          <ButtonLoginSpotify type="button">
+            <SpotifyLogo size={32} color="#00B37E" weight="fill" /> Or sign-in
+            with Spotify
+          </ButtonLoginSpotify>
+        </a>
       </FormLogin>
     </ContainerLogin>
   )
